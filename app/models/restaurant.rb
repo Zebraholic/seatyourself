526663433b96074a47d0d7b0dd8a5bb970694c02
ALISHAN <alishan92@gmail.com>
2014-11-12 17:23:03 -0500

    Defined restaurant and customer models

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,7 @@
 class Restaurant < ActiveRecord::Base
+has_many :customers, through: :reservations
+has_many :reservations
+has_many :reviews
+
+validates :name, :email, :phone, :location, :time_open, :time_closed, :food_type, :capacity, presence: true
 end
