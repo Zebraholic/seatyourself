7140ac63ba2e9f16d570b5180ce7a8b54628ef8b
Zebraholic <rctavares@rogers.com>
2014-11-12 17:42:48 -0500

    Reservation and Review models set up.

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,6 @@
 class Reservation < ActiveRecord::Base
+	belongs_to :restaurant
+	belongs_to :customer
 
-
+validates :size, :time, presence: true
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ActiveRecord::Base
+	belongs_to :customer
+	belongs_to :restaurant
+
+	validates :title, :body, presence: true
 end
