8daa06406810b0dc6cbda4fb2197abd2c9d6e530
Nurbrun <bruno.alfano.rizzo@gmail.com>
2014-11-14 17:34:09 -0500

    added customer and restaurant forms to new-views... Made the root index

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ has_many :customers, through: :reservations
 has_many :reservations
 has_many :reviews
 
-validates :name, :email, :phone, :location, :time_open, :time_closed, :food_type, :capacity, presence: true
+validates :name, :email, :phone, :location, :food_type, :capacity, presence: true
 
 	def available?(size, time)
 		reserved = reservations.where(:time => time).sum(:size)
